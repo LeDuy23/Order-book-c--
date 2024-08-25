91f8ec73355ad108a53221c87772cd321e24bcea
LeDuy23 <143040215+LeDuy23@users.noreply.github.com>
2024-08-25 02:06:01 -0700

    Finish product

---
+++ Order.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <list>
+#include <exception>
+#include <format>
+
+#include "OrderType.h"
+#include "Side.h"
+#include "Usings.h"
+#include "Constants.h"
+
+
+class Order
+{
+public:
+    Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity)
+        : orderType_{ orderType }
+        , orderId_{ orderId }
+        , side_{ side }
+        , price_{ price }
+        , initialQuantity_{ quantity }
+        , remainingQuantity_{ quantity }
+    { }
+
+    Order(OrderId orderId, Side side, Quantity quantity)
+        : Order(OrderType::Market, orderId, side, Constants::InvalidPrice, quantity)
+    { }
+
+    OrderId GetOrderId() const { return orderId_; }
+    Side GetSide() const { return side_; }
+    Price GetPrice() const { return price_; }
+    OrderType GetOrderType() const { return orderType_; }
+    Quantity GetInitialQuantity() const { return initialQuantity_; }
+    Quantity GetRemainingQuantity() const { return remainingQuantity_; }
+    Quantity GetFilledQuantity() const { return GetInitialQuantity() - GetRemainingQuantity(); }
+    bool IsFilled() const { return GetRemainingQuantity() == 0; }
+    void Fill(Quantity quantity)
+    {
+        if (quantity > GetRemainingQuantity())
+            throw std::logic_error(std::format("Order ({}) cannot be filled for more than its remaining quantity.", GetOrderId()));
+
+        remainingQuantity_ -= quantity;
+    }
+    void ToGoodTillCancel(Price price) 
+    { 
+        if (GetOrderType() != OrderType::Market)
+            throw std::logic_error(std::format("Order ({}) cannot have its price adjusted, only market orders can.", GetOrderId()));
+
+        price_ = price;
+        orderType_ = OrderType::GoodTillCancel;
+    }
+
+private:
+    OrderType orderType_;
+    OrderId orderId_;
+    Side side_;
+    Price price_;
+    Quantity initialQuantity_;
+    Quantity remainingQuantity_;
+};
+
+using OrderPointer = std::shared_ptr<Order>;
+using OrderPointers = std::list<OrderPointer>;
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Order book 
- 
+
+
+Order Book Implementation in C++
+
+Overview
+
+This project is a C++ implementation of an Order Book, a core component of trading platforms in financial markets. The order book efficiently manages buy and sell orders, matches them based on price and time priority, and simulates the trading process. This project showcases skills in C++ programming, data structure optimization, and real-time trading logic.
+
+Features
+
+1. Order Management
+Buy and Sell Orders: The system handles and processes incoming buy and sell orders.
+Order Types: Supports different types of orders:
+Market Orders: Executed immediately at the best available price.
+Limit Orders: Executed at a specified price or better.
+FillOrKill Orders: These orders must be filled entirely immediately or canceled (killed) without any partial fills.
+GoodForDay Orders: These orders remain active until the end of the trading day or until they are executed, whichever comes first.
+Order Matching: Automatically matches orders based on price and time priority.
+2. Data Structures
+Efficient Storage: Orders are stored using optimized data structures, such as maps or priority queues, for quick access and updates.
+Price Levels: The order book is organized by price levels, each containing a list of orders at that specific price.
+3. Trading Logic
+Price-Time Priority: Orders are matched based on the best price and the earliest time of arrival if prices are the same.
+Partial Fills: Supports partial order fills, allowing orders to be partially executed if there isn't enough quantity at a given price level (except for FillOrKill orders).
+4. Unit Testing
+Google Test Framework: The project includes comprehensive unit tests written using the Google Test framework. These tests validate the correctness of the order matching logic, data structures, and overall functionality.
+
